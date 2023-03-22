2ec0d4c39c2d3b67f3490fd9990fd874543c82f2
B0P <85157213+0lszowm@users.noreply.github.com>
2023-03-22 23:29:30 +0100

    Poprawione rozwiazanie do cw1 z lab2
    Rozwiazania powinny dzialac, teraz tylko kiedys ogarnac syf w kodzie i bedzie git

--- Cw1/L2_buda.m
+++ Cw1/L2_buda.m
@@ -81,47 +81,78 @@ en_freq_v = rabarbar(v, N, Tp);
 % Parametry
 f = linspace(0,1/(2*Tp),N/2+1); % wektor częstotliwości
 
-% Generowanie sygnału
-e = sigm * randn(1,N);
+baton(0.1, N, Tp, f, 4)
+baton(100, N, Tp, f, 5)
 
-% Estymacja gęstości widmowej mocy - periodogram
-S_per = (1/(N*Tp)) * abs(fft(e)).^2;
-S_per = S_per(1:N/2+1);
+szambo(100, e, 6)
+szambo(2000, e, 7) %% tutaj jest to co nizej dla sygnalu e
 
-% Estymacja gęstości widmowej mocy - korelogram
-[r, lags] = xcorr(e, 'unbiased');
-r = r(N:end);
-lags = lags(N:end);
-S_corr = (1/(N*Tp)) * abs(fft(r)).^2;
-S_corr = S_corr(1:N/2+1);
+szambo(2000, v, 8) %% tutaj przejebane to co wyzej tylko dla sygnału v
 
-% Wykresy
-figure(4);
-subplot(3,1,1);
-plot((0:N-1)*Tp, e);
-xlabel('Czas [s]');
-title('Sygnał e');
-
-subplot(3,1,2);
-plot(f, 10*log10(S_per));
-xlabel('Częstotliwość [Hz]');
-ylabel('Gęstość widmowa mocy [dB/Hz]');
-title('Periodogram');
-
-subplot(3,1,3);
-plot(f, 10*log10(S_corr));
-xlabel('Częstotliwość [Hz]');
-ylabel('Gęstość widmowa mocy [dB/Hz]');
-title('Korelogram');
 
 %% tu znajdują się definicje funkcji
+
+function szambo(N, e, img)
+    %okno i wykreślona estymata gęstości widmowej mocy
+%N=100;
+omega=0:2*pi/N:2*pi-2*pi/N;
+Mh=N/5;
+i=0;
+for tau = 0:N-1
+    i=i+1;
+    if tau<=Mh
+        wh(i)=0.5*(1+cos(tau*pi/Mh));
+    end
+    if tau>Mh
+        wh(i)=0;
+    end
+    r_xx(i)=Covar([e',e'],tau);
+    cor_xxh(i)=wh(i)*r_xx(i)*exp(-1j*omega(i)*tau);
+end
+figure(img)
+stem(omega(1:N/2),cor_xxh(1:N/2))
+end
+
+
+function baton(sigm, N, Tp, f, i)
+    % Generowanie sygnału
+    e = sigm * randn(1,N);
+
+    % Estymacja gęstości widmowej mocy - periodogram
+    S_per = (1/(N*Tp)) * abs(fft(e)).^2;
+    S_per = S_per(1:N/2+1);
+
+    % Estymacja gęstości widmowej mocy - korelogram
+    [r, lags] = xcorr(e, 'unbiased');
+    r = r(N:end);
+    lags = lags(N:end);
+    S_corr = (1/(N*Tp)) * abs(fft(r)).^2;
+    S_corr = S_corr(1:N/2+1);
+
+    % Wykresy
+    figure(i);
+    subplot(3,1,1);
+    plot((0:N-1)*Tp, e);
+    xlabel('Czas [s]');
+    title('Sygnał e');
+
+    subplot(3,1,2);
+    plot(f, 10*log10(S_per));
+    xlabel('Częstotliwość [Hz]');
+    ylabel('Gęstość widmowa mocy [dB/Hz]');
+    title('Periodogram');
+
+    subplot(3,1,3);
+    plot(f, 10*log10(S_corr));
+    xlabel('Częstotliwość [Hz]');
+    ylabel('Gęstość widmowa mocy [dB/Hz]');
+    title('Korelogram');
+end
+
 function x = skakanka(n, tp)
     x = sin(2*pi*5*n*tp) + 0.5*sin(2*pi*10*n*tp) + 0.25*sin(2*pi*30*n*tp);
 end
 
-function en = rabarbar(sig, n, tp)
-    X_Njw = abs(fft(sig, n));
-    X_Njw = 2*X_Njw/(n*tp);
-    X_Njw = X_Njw/(n/2);
-    en = sum(1/(n*tp)*X_Njw.^2);
+function en = rabarbar(sig, ork, tp)
+    en = tp*sum(abs(tp*(fft(sig,ork)).^2)/(ork*tp));
 end
