46afd79fd3db7bb7ee275a2165ce8d9ff48ae7bf
0lszowm <0lszowm@gmail.com>
2023-04-13 18:51:52 +0200

    Dodaje to co zostało zrobione w trakcie zajec
    do poprawy to wszystko jest

--- Cw2/L2_ork.m
+++ Cw2/L2_ork.m
@@ -11,7 +11,7 @@ N = 1001;
 sigma2v = 0;
 tend = 500; %% w sekundach to tez musi byc 
 % to co jest wyzej mozna zmienic dowolnie (chyba)
-
+sim('AKident.mdl')
 
 
 %% tutaj bedzie kod ktory bedzie wyswietlac wykresy
@@ -25,3 +25,38 @@ title("To jest pierwszy wykres") %% tu wpisać co na tym wykresie jest należy
 figure(2)
 plot(Zdata(:, 2), Zdata(:,1), "r.")
 title("To jest drugi wykres") %% tu wpisać co na tym wykresie jest należy
+
+
+%% trzecie polecenie
+% Analiza korelacyjna
+u = Zdata(:,1);
+y = Zdata(:,2);
+t = Zdata(:,3);
+M = 20;
+tau = 0:1:M-1;
+% deklaracja macierzy korelacyjnych
+Ryu = zeros(1, N);
+Ruu = zeros(1, N);
+
+% Wypelnianie macierzy korelacyjnych
+for i = 0:1:(N-1)
+    ruu = Covar([u u], i);
+    %wlasna
+    Ruu(i+1) = ruu;
+    
+    ryu = Covar([y u], i);
+    %wspolna
+    Ryu(i+1) = ryu;
+end
+
+
+%% part3
+
+% deklaracja macierzy est
+Ruu_Hat = [];
+
+% budowanie maceirzy est.
+for i = 1:1:length(20)
+    ruu_Hat = Ruu([20-i+1:20, 1:20-i])'; 
+    Ruu_Hat = [Ruu_Hat; ruu_Hat'];
+end
